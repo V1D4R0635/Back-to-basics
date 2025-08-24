36a8e3d9150a52c0f093fa8723d7c7241495675e
Travis <paculba.christiantravisdwayme@gmail.com>
2025-08-24 19:52:51 +0800

    made a basic calculator

---
+++ C/day4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sol;
+    char op;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    printf("Options: + - * \n Select an operator: ");
+    scanf(" %c", &op);
+
+    if(op == '+'){
+        sol = num1 + num2;
+        printf("%d + %d = %d", num1, num2, sol);
+    }else if(op == '-'){
+        sol = num1 - num2;
+        printf("%d - %d = %d", num1, num2, sol);
+    }else if(op == '*'){
+        sol = num1 * num2;
+        printf("%d * %d = %d", num1, num2, sol);
+    }else if(op == '/'){
+        if(num2 == 0){
+            printf("Please input a number other than 0");
+        } else{
+            sol = num1 / num2;
+        printf("%d / %d = %d", num1, num2, sol);
+        }
+    }else{
+        printf("Please input one of the choices");
+    }
+
+}
